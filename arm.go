44c2cbd99684523389c4d49b62c7345b90151067
Chris Pearce <cpearce@mozilla.com>
2018-10-22 17:38:06 +1300

    Split itemizer out into its own file.

--- arm.go
+++ arm.go
@@ -28,45 +28,6 @@ import (
 // Item represents an item.
 type Item int
 
-// Itemizer converts between a string to an Item type, and vice versa.
-type Itemizer struct {
-	strToItem map[string]Item
-	itemToStr map[Item]string
-	numItems  int
-}
-
-// Itemize converts a slice of strings to a slice of Items.
-func (it *Itemizer) Itemize(values []string) []Item {
-	items := make([]Item, 0)
-	for _, val := range values {
-		val = strings.TrimSpace(val)
-		if len(val) == 0 {
-			continue
-		}
-		itemID, found := it.strToItem[val]
-		if !found {
-			it.numItems++
-			itemID = Item(it.numItems)
-			it.strToItem[val] = itemID
-			it.itemToStr[itemID] = val
-		}
-		items = append(items, itemID)
-	}
-	return items
-}
-
-func (it *Itemizer) cmp(a Item, b Item) bool {
-	return it.itemToStr[a] < it.itemToStr[b]
-}
-
-func newItemizer() Itemizer {
-	return Itemizer{
-		strToItem: make(map[string]Item),
-		itemToStr: make(map[Item]string),
-		numItems:  0,
-	}
-}
-
 func filter(vs []Item, f func(Item) bool) []Item {
 	vsf := make([]Item, 0)
 	for _, v := range vs {
---
+++ itemizer.go
@@ -0,0 +1,42 @@
+package main
+
+import "strings"
+
+// Itemizer converts between a string to an Item type, and vice versa.
+type Itemizer struct {
+	strToItem map[string]Item
+	itemToStr map[Item]string
+	numItems  int
+}
+
+// Itemize converts a slice of strings to a slice of Items.
+func (it *Itemizer) Itemize(values []string) []Item {
+	items := make([]Item, 0)
+	for _, val := range values {
+		val = strings.TrimSpace(val)
+		if len(val) == 0 {
+			continue
+		}
+		itemID, found := it.strToItem[val]
+		if !found {
+			it.numItems++
+			itemID = Item(it.numItems)
+			it.strToItem[val] = itemID
+			it.itemToStr[itemID] = val
+		}
+		items = append(items, itemID)
+	}
+	return items
+}
+
+func (it *Itemizer) cmp(a Item, b Item) bool {
+	return it.itemToStr[a] < it.itemToStr[b]
+}
+
+func newItemizer() Itemizer {
+	return Itemizer{
+		strToItem: make(map[string]Item),
+		itemToStr: make(map[Item]string),
+		numItems:  0,
+	}
+}
